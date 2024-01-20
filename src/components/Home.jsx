86e22ce04e9000a7febd30d8b35cb5ac601f0392
Dee-Codez <debampati44@gmail.com>
2024-01-20 06:01:52 +0530

    3D Fix_1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -38,7 +38,7 @@ const Home = () => {
       </div>
       <div className='absolute mt-[40vh] opacity-70 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blend-overlay -z-50 overflow-hidden text-white'>
         {/* <Spline scene="https://prod.spline.design/MONLQBPmoTPbTsDZ/scene.splinecode" /> */}
-        <Spline scene="https://prod.spline.design/IEnPKKTqk3lQ-3FE/scene.splinecode" />
+        <Spline className='hover:pause' scene="https://prod.spline.design/IEnPKKTqk3lQ-3FE/scene.splinecode" />
         {/* <img src={emblem} width={400} /> */}
       </div>
       <div className='flex justify-center mt-[40vh] gap-[15%]'>
